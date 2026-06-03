5283ec7cbb642e8b8b0eae2cb7a5a86052ff9956
Ian Miller <ianmiller093@gmail.com>
2026-06-03 11:33:46 -0400

    clearer comments and removed remnants of assumptions of being a sphere

--- general_plot_power_spectrum.py
+++ general_plot_power_spectrum.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius_degrees, fullsky_bool=False):
+# Plots the power spectra of TT, EE, and BB of a segment of given size of a given simulated PySM sky
+# Assumes segment is small enough to be assumed as rectangular
+def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius_degrees):
 
     # making the shape so I can take a piece out of the sky without looking at the whole sky
     shape, wcs = enmap.geometry(
@@ -23,46 +25,37 @@ def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius
         [(dec_center_degrees + radius_degrees) * utils.degree,
         (ra_center_degrees  - radius_degrees) * utils.degree]],
 
-        # fixed
+        # using nside2resol instead of calculating it myself
         res = hp.pixelfunc.nside2resol(sky.nside, arcmin=True) * utils.arcmin,
         proj="car"
     )
 
-    # initialize imap_hp from sky
+    # initialize imap_hp of the given frequency from sky
     imap_hp = sky.get_emission(f_GHz * u.GHz).value
 
-    # reproject to get only a tiny segment
+    # isolate only the small segment of the sky
     imap = reproject.healpix2map(imap_hp, shape, wcs)
 
     # add some random noise?  Gaussian noise taken from matched filtering notebook
-    # definitely ask Zach about this
-    # gonna comment this out briefly, I wanna double check and see if the power spectrum is more normal w/o this
-    # only some minor things came from commenting it out, to be expected, may as well keep.
     pixarea = enmap.pixsizemap(shape, wcs)
     ivar = 10**-2*pixarea/utils.arcmin**2
     for i in range(3):
         noise = enmap.rand_gauss(shape, wcs) * np.sqrt(1/ivar)
         imap[i] += noise
     
-
-    # apodize if not fullsky
-    # I'm preeeeeeeetty sure you don't have to apodize if it's the full sky?  Cuz it loops around right?
-    # if we don't apodize, then w2 ends up being 1 by default, and tapered_imap is just a copy of imap
-    w2 = 1
     tapered_imap = imap.copy()
-    
-    if not fullsky_bool:
-        # deleted the three lines I was unsure of for this...
-        apod_width = max(5, shape[1] // 8)
-        # from spherical harmonics notebook
-        taper_mask = enmap.apod(enmap.ones(imap[0].shape, imap[0].wcs), width=apod_width)
-        # removed the division by 4 \pi in the formula...
-        w2 = np.mean(taper_mask**2)
-        for i in range(3):
-            tapered_imap[i] = taper_mask * imap[i]
+    # calculate the width of the taper mask based on the size of the 
+    apod_width = max(5, shape[1] // 8)
+    taper_mask = enmap.apod(enmap.ones(imap[0].shape, imap[0].wcs), width=apod_width)
+    # removed the division by 4 \pi in the formula, no longer assuming that this is over a sphere
+    w2 = np.mean(taper_mask**2)
+    for i in range(3):
+        tapered_imap[i] = taper_mask * imap[i]
 
     # lmax is no longer arbitrarily large, changed it to have a set formula
     lmax = 2 * sky.nside
+
+    # Still using sphere stuff under the hood for alms, even if w2 calculations assume = rectangular
     alms = curvedsky.map2alm(tapered_imap, lmax=lmax)
     cl = curvedsky.alm2cl(alms) / w2
     l = np.arange(cl.shape[1])
