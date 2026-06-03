926616daf2513a87e5093b4604eea2acc64bbbc9
Ian Miller <ianmiller093@gmail.com>
2026-06-03 10:43:25 -0400

    several changes to resolution calculations and plotting

--- sims_at_multiple_frequencies.py
+++ sims_at_multiple_frequencies.py
@@ -17,22 +17,24 @@ def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius
 
     # making the shape so I can take a piece out of the sky without looking at the whole sky
     shape, wcs = enmap.geometry(
-        # I like triple checked to make sure dec and ra are correct and the +- signs are right but idk
-        pos=[[(ra_center_degrees - radius_degrees) * utils.degree, 
-        (dec_center_degrees + radius_degrees) * utils.degree], 
-        [(ra_center_degrees + radius_degrees) * utils.degree, 
-        (dec_center_degrees - radius_degrees) * utils.degree]], 
+        # I like triple checked to make sure dec and ra are correct and the +- signs are right -fixed
+        pos=[[(dec_center_degrees - radius_degrees) * utils.degree,
+        (ra_center_degrees  + radius_degrees) * utils.degree],
+        [(dec_center_degrees + radius_degrees) * utils.degree,
+        (ra_center_degrees  - radius_degrees) * utils.degree]],
 
-        # conversion factor of 1/3600 for sky.nside seeeeems to be correct?
-        res = (3600 / sky.nside) * utils.arcmin,
+        # fixed
+        res = hp.pixelfunc.nside2resol(sky.nside, arcmin=True) * utils.arcmin,
         proj="car"
     )
 
     # initialize imap_hp from sky
     imap_hp = sky.get_emission(f_GHz * u.GHz).value
+    print(imap_hp.shape)
 
     # reproject to get only a tiny segment
     imap = reproject.healpix2map(imap_hp, shape, wcs)
+    print(imap.shape)
 
     # add some random noise?  Gaussian noise taken from matched filtering notebook
     # definitely ask Zach about this
@@ -50,19 +52,28 @@ def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius
     # if we don't apodize, then w2 ends up being 1 by default, and tapered_imap is just a copy of imap
     w2 = 1
     tapered_imap = imap.copy()
+    
     if not fullsky_bool:
         # deleted the three lines I was unsure of for this...
         apod_width = max(5, shape[1] // 8)
         # from spherical harmonics notebook
         taper_mask = enmap.apod(enmap.ones(imap[0].shape, imap[0].wcs), width=apod_width)
-        w2 = np.sum(taper_mask.pixsizemap() * taper_mask**2) / (4*np.pi)
+        # removed the division by 4 \pi in the formula...
+        w2 = np.mean(taper_mask**2)
         for i in range(3):
             tapered_imap[i] = taper_mask * imap[i]
 
-    # not so sure about lmax=4000?  I tried to make it arbitrarily large?
-    alms = curvedsky.map2alm(tapered_imap, lmax=4000)
+    print(tapered_imap.shape)
+
+    # lmax is no longer arbitrarily large, changed it to have a set formula
+    lmax = 2 * sky.nside
+    alms = curvedsky.map2alm(tapered_imap, lmax=lmax)
     cl = curvedsky.alm2cl(alms) / w2
     l = np.arange(cl.shape[1])
+
+    print(alms.shape)
+    print(cl.shape)
+    print(l.shape)
 
     for i in range(3):
         plt.semilogy(l, cl[i] * l *(l+1) / 2 / np.pi, label=['TT', 'EE', 'BB'][i])
@@ -73,5 +84,6 @@ def show_power_spectra(sky, f_GHz, ra_center_degrees, dec_center_degrees, radius
     plt.close()
 
 # testing it out with sky with d0 dust and CMB
-sky = pysm3.Sky(nside=256, preset_strings=["d0", "c1"])
-show_power_spectra(sky, 100, 0, 0, 1, False)
+sky = pysm3.Sky(nside=512, preset_strings=["d0", "c1"])
+# r = 1 was too small, I think, it led to a 9x9 map lol
+show_power_spectra(sky, 100, 0, 0, 5, False)
