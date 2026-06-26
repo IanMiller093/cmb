2680a42ba02f3ed731ccb3a9f7e430b3a9ed364c
Ian Miller <ianmiller093@gmail.com>
2026-06-26 16:49:17 -0400

    50% done new cmb_and_foreground

--- cmb_and_foreground.py
+++ cmb_and_foreground.py
@@ -2,9 +2,13 @@ from pixell_cmb import make_cmb
 from pysm_foreground import make_foreground
 from act_planck_beam import apply_beam
 from act_planck_noise import accurate_noise
-
-from pixell import enmap, utils
+import pysm3
+import pysm3.units as u
+import healpy as hp
 import numpy as np
+from pixell import enmap, reproject, utils
+from bandpass import bandpass   
+
 
 def make_cmb_and_foreground(dec_radius=90, ra_radius=180, seed=67, res=1, sky_f=150, foreground_components=["d0"], include_noise=True, beam=True, rot=False, bp=True, bb_telescope="planck", bb_channel=100, bb_pa=None, beam_type="jitter_cmb", beam_split="coadd"):
     '''
@@ -32,3 +36,130 @@ def make_cmb_and_foreground(dec_radius=90, ra_radius=180, seed=67, res=1, sky_f=
         result += accurate_noise(telescope=bb_telescope, channel=bb_channel, shape=shape, wcs=wcs, pa=bb_pa)
 
     return result
+
+
+'''
+new cmb_and_foreground architecture below!!!
+'''
+
+# constants yay
+h_P = 6.626e-34
+k_B = 1.381e-23
+T_CMB = 2.725 
+
+# sed scaling function for cmb
+def cmb_sed_scaling(nu):
+    return 1.0
+
+# sed scaling function for dust
+def dust_sed_scaling(nu, nu_0, beta, T_dust):
+    nu_hz = nu   * 1e9
+    nu_0_hz = nu_0 * 1e9
+    x = h_P * nu_hz / (k_B * T_dust)
+    x0 = h_P * nu_0_hz / (k_B * T_dust)
+    planck_ratio = (nu_hz / nu_0_hz)**3 * np.expm1(x0) / np.expm1(x)
+    return (nu_hz / nu_0_hz)**(beta + 1) * planck_ratio
+
+def rj_to_cmb_factor(nu):
+    x  = h_P * nu * 1e9 / (k_B * T_CMB)
+    ex = np.exp(x)
+    return x**2 * ex / (ex - 1)**2
+
+def hp_to_car_wrapper(hp_map, shape, wcs, rot=False):
+    
+    rot_str = "gal,cel" if rot else None
+    car = reproject.healpix2map(hp_map, shape[-2:], wcs, rot=rot_str)
+    return np.array(car).flatten()
+
+def bandpass_sed_dust(bp_freqs, bp_weights, nu_0, beta_map, T_dust_map):
+
+    # normalize weights so trapz integrates to 1
+    norm = np.trapz(bp_weights, bp_freqs)
+    bp_weights_normed = bp_weights / norm
+
+    # integrand shape: (N_freq, N_pix)
+    integrand = np.zeros((len(bp_freqs), len(beta_map)))
+
+    for i, nu in enumerate(bp_freqs):
+        mbb = dust_sed_scaling(nu, nu_0, beta_map, T_dust_map)       # (N_pix,)
+        integrand[i, :] = mbb / rj_to_cmb_factor(nu) * bp_weights_normed[i]
+
+    return np.trapz(integrand, bp_freqs, axis=0)    # (N_pix,)
+
+
+def bandpass_sed_cmb(bp_freqs, bp_weights):
+
+    norm = np.trapz(bp_weights, bp_freqs)
+    bp_weights_normed = bp_weights / norm
+
+    integrand = np.array([
+        bp_weights_normed[i] / rj_to_cmb_factor(nu)
+        for i, nu in enumerate(bp_freqs)
+    ])
+
+    return np.trapz(integrand, bp_freqs)
+
+
+
+# TODO: generalize to not fullsky?
+# TODO: wait what do I do about generating the map with one freq?
+
+'''
+NEW lacrosse pipeline segue finance: 
+cmb w/o beam -> 
+dust w/o beam... w/ bandpass? -> 
+stack together -> 
+apply beams to channels of the same frequency??? ->
+add noise ->
+return T and d
+
+Currently generalized to just look at intensity
+'''
+def new_cmb_and_foreground(freqs, dust_list=["d1"], res_arcmin=1, beam=True, seed=67, beam_telescope="act", beam_pas=None, flatsky=False):
+
+    # N_comp * N_chan is the number of individual maps we'll have
+    # number of freq channels
+    N_chan = len(freqs)
+    # for cmb and dust
+    N_comp = 2
+
+    sky_nside  = 1
+    while hp.nside2resol(sky_nside, arcmin=True) > res_arcmin:
+        sky_nside *= 2
+
+    sky = pysm3.Sky(nside=sky_nside, preset_strings=dust_list)
+    dust_model = sky.components[0]
+
+    a_cmb = make_cmb(dec_radius=90, ra_radius=180, seed=seed, res=res_arcmin, beam=beam, beam_telescope=beam_telescope, flatsky=flatsky)
+    shape = a_cmb.shape
+    wcs = a_cmb.wcs
+
+    a_dust = dust_model.map_I.value.squeeze()
+    beta_dust = dust_model.mbb_index.value.squeeze()
+    T_dust = dust_model.mbb_temperature.value.squeeze()
+    nu_0_dust = dust_model.freq_ref_I.to("GHz").value
+
+    a_dust = hp_to_car_wrapper(a_dust, shape, wcs, rot=False)
+    beta_dust = hp_to_car_wrapper(beta_dust, shape, wcs, rot=False)
+    T_dust = hp_to_car_wrapper(T_dust, shape, wcs, rot=False)
+
+    a_cmb_1d = np.array(a_cmb[0]).flatten()
+
+    N_pix = len(a_cmb_1d)
+    assert len(a_dust) == N_pix, "pixel count mismatch after reprojection"
+
+    bandpasses = [bandpass(telescope=beam_telescope, channel=nu) for nu in freqs]
+
+    # actual construct ts T now!!!  Tensor with a bunch of maps as opposed to just one huge matrix
+    T = np.zeros((N_chan, N_comp, N_pix))
+
+    for f_index, (bp_freqs, bp_weights) in enumerate(bandpasses):
+        T[f_index, 0, :] = bandpass_sed_cmb(bp_freqs, bp_weights)
+        T[f_index, 1, :] = bandpass_sed_dust(bp_freqs, bp_weights, nu_0_dust, beta_dust, T_dust)
+    
+    a = np.vstack([a_cmb_1d, a_dust]) 
+
+    d_tensor = np.einsum('fcp,cp->fp', T, a)
+    d_vector = d_tensor.flatten()
+
+    return d_vector
